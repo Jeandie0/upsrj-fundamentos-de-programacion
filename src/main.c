0b56ddc7442027c6c4f6d4dce6ced400c5eab8e8
Jeandie0 <jeigocoronab@gmail.com>
2026-01-25 13:41:32 -0600

    Programa que  determina par e impar.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
-{
-    printf("Hello world!\n");
+int is_even(int x){
+        if(x % 2 == 0){
+            return 1;
+        }
+        else if(x % 2 == 1){
+            return 0;
+        }
+}
+int main(){
+    int num;
+    printf("Ingrese un numero:");
+    scanf("%i", &num);
+    if(num == 0){
+        printf("You can not divide by 0 \n");
+    }
+    else if(is_even(num) == 1){
+        printf("El numero es par\n");
+    }
+    else if(is_even(num) == 0){
+        printf("El numero es impar\n");
+    }
     return 0;
 }
